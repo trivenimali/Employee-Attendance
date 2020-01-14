543076f9761d8cc56c147fe1410b557431bafdaf
trivenimali <58415561+trivenimali@users.noreply.github.com>
2020-01-14 16:03:10 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
    firebaseConfig :{
-    apiKey: "AIzaSyCHisKWCCNtYbhD9eFe85CoWi3lpVXSSQA",
-    authDomain: "zimply-d7f16.firebaseapp.com",
-    databaseURL: "https://zimply-d7f16.firebaseio.com",
-    projectId: "zimply-d7f16",
-    storageBucket: "zimply-d7f16.appspot.com",
-    messagingSenderId: "497931845829",
-    appId: "1:497931845829:web:303f21b8857df8754ef0ac",
-    measurementId: "G-SKH8BQCRYM"
+    
   }
 
 };
